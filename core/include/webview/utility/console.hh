b7b67246644f3afc31b787d55b95cbdd49e7aaef
citkane <michael@openpoint.ie>
2025-03-29 19:52:31 +0000

    Update console for Windows use

--- core/include/webview/utility/console.hh
+++ core/include/webview/utility/console.hh
@@ -1,50 +1,54 @@
 #pragma once
-#if defined(__cplusplus) && !defined(WEBVIEW_HEADER)
 
+#if defined(__cplusplus) && !defined(WEBVIEW_HEADER)
 #include <string>
 
-#if defined(_WIN32)
-struct win_console_t {
-  int ex_stdout;
-  int ex_stderr;
-  bool has_console;
-};
-#endif // defined(_WIN32)
-
-struct colors_t {
+struct colours_t {
   int yellow;
+  int red;
 };
 
 namespace webview {
 namespace utility {
 
+/// A static utility class to log colourised warnings and errors to the process console.
 class console {
 public:
+  /// Logs a warning in yellow to stdout
   static void warn(std::string message);
-  /** TODO
-   *
-   * Add appropriately colorised console methods for future
-   * refactoring into Webview 
-   *
-   * static void error(std::string &message);
-   * static void log(std::string &message);
-   */
+
+  /// Logs an error in red to stderr
+  static void error(std::string message);
 
 private:
-  static bool get_console();
+  /// Performs console initialisation differently for Windows and *Nix like systems
+  static void init_console();
 
-  static void release_console();
+  /// Frees the console for Windows systems else the process will hang/error at exit.
+  static void free_console();
 
-  template <typename Color>
-  static std::string set_color(Color color, std::string message);
+  /// Sets an appropriate colour to a message string.
+  static std::string set_colour(int color, std::string message);
 
-  static colors_t colors;
+  /// UTF console colour code int values.
+  static colours_t colours;
+
+  // Flags if the console ASCI escape mode was successfully set.
+  // This is always true for *Nix systems.
+  static bool is_console_mode_set;
 
 #if defined(_WIN32)
-  static win_console_t win_console;
-#endif // defined(_WIN32)
-};
 
+  /// Determines if the user has already attached a console to the process.
+  /// This function is lazy initialised with a local static variable cached.
+  static bool user_has_console();
+
+  /// Flags if Webview is in ownership of an attached console
+  static bool wv_has_console;
+
+#endif // defined(_WIN32)
+
+}; // class console
 } // namespace utility
 } // namespace webview
 
--- core/src/utility/console.cc
+++ core/src/utility/console.cc
@@ -1,68 +1,106 @@
 #pragma once
-#include <iostream>
 #if defined(__cplusplus) && !defined(WEBVIEW_HEADER)
-
 #include "webview/utility/console.hh"
+#include <iostream>
 
 using namespace webview::utility;
 
 void console::warn(std::string message) {
-  if (!get_console()) {
-    return;
-  };
+  init_console();
   std::string prefix = "[WEBVIEW][WARNING]: ";
-  message = set_color(colors.yellow, prefix + message);
+  message = set_colour(colours.yellow, prefix + message);
   printf("%s\n", message.c_str());
-  release_console();
+  free_console();
 };
 
-bool console::get_console() {
-#if defined(_WIN32)
-  if (!win_console.has_console) {
-    return false;
-  }
-  freopen("CONOUT$", "w", stdout);
-  freopen("CONOUT$", "w", stderr);
-  std::ios::sync_with_stdio(true);
-  return true;
-#else
-  std::cout.flush();
-  return true;
-#endif // defined(_WIN32) #else
+void console::error(std::string message) {
+  init_console();
+  std::string prefix = "[WEBVIEW][ERROR]: ";
+  message = set_colour(colours.red, prefix + message);
+  std::cerr << message << "\n";
+  free_console();
 };
 
-void console::release_console() {
-#if defined(_WIN32)
-  if (!win_console.has_console) {
-    return;
-  }
-  _dup2(win_console.ex_stdout, _fileno(stdout));
-  _dup2(win_console.ex_sterr, _fileno(stderr));
-  _close(win_console.ex_stdout);
-  _close(win_console.ex_sterr);
-  FreeConsole();
-#endif // defined(_WIN32)
-};
-
-#if defined(_WIN32)
-
-win_console_t console::win_console {
-  _dup(_fileno(stdout));
-  _dup(_fileno(stderr));
-  AttachConsole(ATTACH_PARENT_PROCESS)
-};
-
-void console::print(C S::*color, const char *message) {};
-
-#else
-
-colors_t console::colors{33};
-
-template <typename Color>
-std::string console::set_color(Color color, std::string message) {
+std::string console::set_colour(int color, std::string message) {
+  if (!is_console_mode_set) {
+    return message;
+  };
   message = "\x1B[" + std::to_string(color) + "m" + message + "\033[0m";
   return message;
 }
+
+colours_t console::colours{33, 31};
+
+#if defined(_WIN32)
+
+#include <handleapi.h>
+#include <io.h>
+#include <windows.h>
+
+void console::init_console() {
+  std::cout.flush();
+  std::cerr.flush();
+  if (user_has_console()) {
+    return;
+  }
+  /* 
+   * We attach/free the console after every log operation to avoid deeply embedding the console system into 
+   * the Webview class destructor. 
+   * Console output should be minimal, and performance overhead for this negligable.
+   */
+  AttachConsole(ATTACH_PARENT_PROCESS);
+
+  if (wv_has_console) {
+    return;
+  }
+  auto std_out = freopen("CONOUT$", "w", stdout);
+  auto std_err = freopen("CONOUT$", "w", stderr);
+  /*
+   * Below here we enable virtual terminal processing to attempt getting ASCI escaped colours in legacy
+   * Windows systems.
+   */
+  auto fd_out = _fileno(std_out);
+  auto fd_err = _fileno(std_err);
+  auto h_out = reinterpret_cast<HANDLE>(_get_osfhandle(fd_out));
+  auto h_err = reinterpret_cast<HANDLE>(_get_osfhandle(fd_err));
+  DWORD dwMode = 0;
+  auto stat_out =
+      SetConsoleMode(h_out, dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+  auto stat_err =
+      SetConsoleMode(h_err, dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+
+  is_console_mode_set = stat_out && stat_err;
+
+  wv_has_console = true;
+};
+
+void console::free_console() {
+  if (!user_has_console()) {
+    FreeConsole();
+  }
+};
+
+bool console::user_has_console() {
+  static const bool has_console = [] {
+    return GetConsoleWindow() != nullptr;
+  }();
+  return has_console;
+}
+
+bool console::wv_has_console{};
+
+bool console::is_console_mode_set{};
+
+#else
+
+void console::init_console() {
+  std::cout.flush();
+  std::cerr.flush();
+};
+
+void console::free_console() {}
+
+bool console::is_console_mode_set = true;
 
 #endif // defined(_WIN32) #else
 #endif // defined(__cplusplus) && !defined(WEBVIEW_HEADER)
