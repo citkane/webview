7fba805bd9f27e7deb20d4de5b5479fbf67c0464
citkane <michael@openpoint.ie>
2025-05-03 20:25:19 +0100

    Fix amalgamation test apps

--- scripts/amalgamate/test/main.c
+++ scripts/amalgamate/test/main.c
@@ -5,7 +5,7 @@ static void terminate_webview(webview_t w) { webview_terminate(w); }
 
 int main(void) {
   webview_t w = webview_create(0, NULL);
-  webview_dispatch(w, terminate_webview(w));
+  webview_dispatch(w, terminate_webview, w);
   webview_run(w);
   webview_destroy(w);
   return 0;
